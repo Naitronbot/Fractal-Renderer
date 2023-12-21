cba6a57d7ac6c13ae07a06a4767c9997fa640536
Naitronbot <lackpup@gmail.com>
2023-12-21 13:49:48 -0800

    Add mathquill dependency

--- src/fractal-settings/FractalSettings.svelte
+++ src/fractal-settings/FractalSettings.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+  import "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
+  import "/mathquill/mathquill-basic.min.js";
+</script>
+
 <link rel="stylesheet" href="./FractalSettings.css" />
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import Main from "./Main.svelte";
+import "./global.css";
+import "/mathquill/mathquill-basic.css";
 
 const main = new Main({
   target: document.getElementById("Main")!,
